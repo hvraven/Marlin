44ff91160fe4ff529148e30342db3365329560d0
Hendrik v. Raven <hendrik@consetetur.de>
2020-01-16 14:17:07 +0100

    optimised extruder PID parameters

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -475,9 +475,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 24.03
+  #define DEFAULT_Ki 1.98
+  #define DEFAULT_Kd 72.84
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
