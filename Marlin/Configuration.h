e0ce3530e86704012896c864fe7aa456d4cb74bb
Hendrik v. Raven <hendrik@consetetur.de>
2020-02-08 12:57:31 +0100

    update PID parameters

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -475,9 +475,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 22.11
+  #define DEFAULT_Ki 1.77
+  #define DEFAULT_Kd 69.20
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
@@ -531,9 +531,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 204.75
+  #define DEFAULT_bedKi 40.91
+  #define DEFAULT_bedKd 683.18
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
